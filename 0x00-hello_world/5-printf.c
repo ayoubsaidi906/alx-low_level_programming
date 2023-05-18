9917051cf29b7adf36ed649a232b3b297b52f7f6
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-18 12:46:07 -0700

    jhvdc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-/* thi is your main fonction */ 
-int main(void)
-{
+/* thi is your main fonction */
+int main(void){
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
