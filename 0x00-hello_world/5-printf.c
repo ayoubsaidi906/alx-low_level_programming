c5e8adf1c78412fb890654ae246f11fa901b7177
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-18 12:54:23 -0700

    jhdbs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-/* thi is your main fonction */
 int main(void)
 {
-/* thitss uygdjkhskuygwshbwkuhduyeguenfiuwrhfuhqwfowiedodjiduhjnhji main */
+/* This is the main function that prints a sentence with proper grammar */
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
