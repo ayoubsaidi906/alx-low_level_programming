f07a1ac39575fdeffbe26883eeb1a1255d75312a
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-18 12:51:36 -0700

    jgfsc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,7 @@
 /* thi is your main fonction */
 int main(void)
 {
+/* thitss uygdjkhskuygwshbwkuhduyeguenfiuwrhfuhqwfowiedodjiduhjnhji main */
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
