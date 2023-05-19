4bb008e7528d6b1105d0ab2a4f307ad1cf9e29b7
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-19 15:19:52 -0700

    kdhbc

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,13 +20,20 @@ n = rand() - RAND_MAX / 2;
 printf("Random number: %d\n", n);
 
 if (n > 0)
+
+{
 printf("%d is positive \n", n);
+}
 
 else if (n < 0)
+{
 printf(" %d is negative\n", n);
+}
 
 else
+{
 printf(" %d is rero\n", n);
+}
 
 return (0);
 }
