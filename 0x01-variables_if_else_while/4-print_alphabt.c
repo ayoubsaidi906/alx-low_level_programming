dd985ea6e21ec832de257fabd4e1d8b475c283d1
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-20 16:58:21 -0700

    jgvwgbsxh

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - is opjectfe
+ *
+ * return 0 is 0 is0
+ */
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i != 'q' && i != 'e')
+{
+putchar(i);
+}
+}
+
+putchar('\n');
+
+return (0);
+}
