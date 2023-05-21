90432c4bbba355c979cfa6e096eace294886e8e0
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-21 14:54:37 -0700

    jgvwgbsxh

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints all the lowercase hexadecimal numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+
+for (i = 'a'; i <= 'f'; i++)
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
