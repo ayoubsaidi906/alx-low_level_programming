ad63cf50505a230255034889ce96f22bc93eef92
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-21 14:37:43 -0700

    jgvwgbsxh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a sentence with proper grammar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char i;
+
+for (i = 'z'; i >= 'a'; i++)
+
+{
+putchar(i);
+}
+
+putchar('\n');
+
+return (0);
+}
