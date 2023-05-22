92bb4e43f6482a00bd0ea486440c97c96dd57f34
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-22 15:19:34 -0700

    jgvwgbsxh

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,26 +4,29 @@
  *
  * descreption: thes programme Write a program that prints all possible differ
  *
- * return : 0 always (Succese)
+ * return : 0 always (Succes)
  */
 
-int main(void) {
-    int i, j;
-    
-    for (i = 0; i < 10; i++) {
-        for (j = i + 1; j < 10; j++) {
-            putchar('0' + i);
-            putchar('0' + j);
-            
-            if (i < 9 || j < 8) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    
-    putchar('\n');
-    
-    return (0);
+int main(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+
+if (i < 9 || j < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
 }
 
+putchar('\n');
+
+return (0);
+}
