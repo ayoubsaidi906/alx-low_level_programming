a78d723f2f5abe39f6c99d41c1ed7e7a96063924
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-22 14:52:33 -0700

    jgvwgbsxh

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,8 +16,8 @@ for (i = 0; i <= 8; i++)
 for (j = 1 + i; j <= 9; j++)
 {
 
-putchar('0' + i)
-putchar('0' + j)
+putchar('0' + i);
+putchar('0' + j);
 {
 
 if (i != 8 || j != 9)
@@ -30,6 +30,7 @@ if (j == 9)
 {
 i++;
 i + j;
+}
 }
 }
 }
