1af2715711dfe408d7b6e8621008d561f64d06cc
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-23 15:58:06 -0700

    ayoub saidi hhhhhandhhhhh

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-return (0);
+	print_alphabet(kmkmkmlkmklmkml);
+	return (0);
 }
