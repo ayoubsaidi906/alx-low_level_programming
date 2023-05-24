d6644c910958ef6854ef74e7f4fdc592c37732e8
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-24 14:16:12 -0700

    ayoub saidi hhhhhandhhhhh

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if @c is lowercase, 0 otherwise.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar("+");
+return (1);
+}
+else if (n == 0)
+{
+_putchar("0");
+return (0);
+}
+else
+{
+_putchar("-");
+return (-1);
+}
+}
