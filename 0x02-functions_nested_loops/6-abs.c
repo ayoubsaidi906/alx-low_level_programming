dd2a471d48d014e6bd72638a1dec885ed918baf8
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-24 16:41:51 -0700

    ayoub saidi hhhhhandhhhhh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - Checks if a character is lowercase.
+ * @n: The character to be checked.
+ *
+ * Return: -n if @n is lowercase, 0 otherwise.
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return -n;
+}
+else
+{
+return n;
+}
+}
