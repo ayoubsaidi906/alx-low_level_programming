a2beea3a93600b947f88b958be8b19bbe38ac08b
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-24 17:09:15 -0700

    ayoub saidi hhhhhandhhhhh

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - Checks if a character is lowercase.
+ * @n: The character to be checked.
+ *
+ * Return: -n if @n is lowercase, 0 otherwise.
+ */
+int print_last_digit(int n)
+{
+int i = n % 10;
+_putchar(i);
+return (i);
+}
