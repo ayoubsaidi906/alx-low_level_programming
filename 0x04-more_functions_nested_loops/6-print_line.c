25d140cde8a0cf6399e95e72c0000f0bfd85d221
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-26 16:08:20 -0700

    0x04-mor fun

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ * @n: The number times the charcter '_' should be printed
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+while (n > 0)
+{
+_putchar('-');
+n--;
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@
 *void prototypes (void)
 *int prototypes (int)
 */
-
+void print_line(int n);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
