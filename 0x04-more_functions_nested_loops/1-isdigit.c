22f8bdcb66b4a3ecfb3a0760cb096fae6b912f71
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-25 18:19:43 -0700

    0x04-mor fun

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a _isdigit or not
+ * @c: digit to be tested
+ *
+ * Return: 1 if thecharacter is digit, 0 otherwise and god look for me
+ */
+
+int _isdigit(int c)
+{
+if ((c >= 0) && (c <= 9))
+{
+return (1);
+}
+
+return (0);
+}
