be87b5b112d3595e15e7141fd40b227976c28fce
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-26 04:22:29 -0700

    0x04-mor fun

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - function that multiplies two integers
+ * @a: character to be tested
+ *
+ * @b: character to be tested
+ *
+ * Return: (a * b) hot fix hhh
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
