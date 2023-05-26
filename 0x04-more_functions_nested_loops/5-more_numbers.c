21ee8f33d31e61014a70549bb497be53873f199f
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-26 15:33:38 -0700

    0x04-mor fun

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,16 +5,16 @@
  */
 void more_numbers(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j <= 14; j++)
-        {
-            if (j >= 10)
-                _putchar('0' + (j / 10));
-            _putchar('0' + (j % 10));
-        }
-        _putchar('\n');
-    }
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+_putchar('0' + (j / 10));
+_putchar('0' + (j % 10));
+}
+_putchar('\n');
+}
 }
