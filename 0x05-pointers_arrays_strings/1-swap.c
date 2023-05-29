5225d40730f3575bc58d6203704c2324d7693dda
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-29 15:04:46 -0700

    whay ar you hard hhh

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * void swap_int - is function that swap to valiuo
+ * swap_int - is function that swap to valiuo
  * @a:is int 1
  * @b:is int 0
  */
- void swap_int(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 int ayoub_me_hhh;
 ayoub_me_hhh = *a;
