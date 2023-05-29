a5a77974c47047d5213462983bb4cec8ea78c602
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-29 15:44:12 -0700

    whay ar you hard hhh

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - is function thet retourn the length of a string
+ * @s: char
+ * @l: uvjhbkbjjbh
+ * Return : to l
+ */
+
+int _strlen(char *s)
+{
+int l = 0;
+
+if (s != 0)
+{
+while (*s != '\0')
+{
+l++;
+s++;
+}
+}
+
+return (l);
+}
