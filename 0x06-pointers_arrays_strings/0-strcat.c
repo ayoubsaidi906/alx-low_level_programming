9b7c5e4a8f307aa8c07542cf1f3994378458f7fb
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-05-31 11:25:21 -0700

    i can

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @*dest: is punter
+ * @*src:is puntters
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src) {
+char *ptr = dest;
+
+while (*ptr != '\0') {
+ptr++;
+}
+
+while (*src != '\0') {
+*ptr = *src;
+ptr++;
+src++;
+}
+
+
+*ptr = '\0';
+
+return dest;
+}
