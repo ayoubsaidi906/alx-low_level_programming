eac85c3350f3b2bd33e4585daf6025d4b21fee74
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-06-02 03:16:38 -0700

    i can

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+/**
+ * cap_string - function
+ * @str: array to compart
+ * Return:str
+ */
+
+char *cap_string(char *str)
+{
+int i, capitalize_next;
+
+capitalize_next = 1;
+
+for (i = 0; str[i] != '\0'; i++) {
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+{
+capitalize_next = 1;
+}    
+else if (capitalize_next && islower(str[i])) {
+str[i] = toupper(str[i]);
+capitalize_next = 0;
+}
+}
+
+return (str);
+}
