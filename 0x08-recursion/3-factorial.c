56879a46ea913add6ded1a0cdf6b029bec9f00e5
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-06-08 03:46:03 -0700

    if you can

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * factorial - count facturial
+ * @n: int
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
