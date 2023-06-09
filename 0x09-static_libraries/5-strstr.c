9709b93cd17f47bf493aaa2d59bdfec814d82dd3
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-06-09 16:48:05 -0700

    my lib

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: string
+ * @needle: string
+ *
+ * Return: string or 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0;
+
+if (*needle == 0)
+return (haystack);
+while (*haystack)
+{
+if (*(haystack + i) == *(needle + i))
+{
+while (*(needle + i) == *(haystack + i))
+{
+if (*(needle + (i + 1)) == 0)
+{
+return (haystack);
+}
+i++;
+}
+}
+haystack++;
+}
+return (0);
+}
