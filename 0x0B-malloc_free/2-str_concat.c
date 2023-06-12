50e252fa2561430392f2af9922ca247911472f74
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-06-12 15:51:59 -0700

    jsqbxh

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: String
+ * @s2: Strng
+ *
+ * Return: null if size = 0 or String
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *p;
+int j = 0, i = 0, len_s1 = 0, len_s2 = 0;
+
+if (s1 != 0)
+{
+while (*(s1 + len_s1))
+len_s1++;
+}
+if (s2 != 0)
+{
+while (*(s2 + len_s2))
+len_s2++;
+}
+p = malloc(len_s1 + len_s2 + 1);
+if (p == 0)
+return (0);
+while (i < len_s1)
+{
+p[i] = *(s1 + i);
+i++;
+}
+while (j < len_s2)
+{
+p[i] = *(s2 + j);
+i++;
+j++;
+}
+return (p);
+}
