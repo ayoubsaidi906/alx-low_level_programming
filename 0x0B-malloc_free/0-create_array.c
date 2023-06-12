035ce5dabb26d15fcbb661fe576ff77729894f6f
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-06-12 05:54:58 -0700

    iss

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,11 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *n = malloc(size);
-	if (size == 0 || n == 0)
-		return (0);
+char *n = malloc(size);
+if (size == 0 || n == 0)
+return (0);
 
-	while (size--)
-		n[size] = c;
-	return (n);
+while (size--)
+n[size] = c;
+return (n);
 }
-
